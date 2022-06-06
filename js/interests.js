769b43d34939f24b198fc72dd9030faacec4040f
RazorHollow <rob@razorhollow.com>
2022-06-06 14:24:19 -0400

    Add about me section

---
+++ js/interests.js
@@ -0,0 +1,34 @@
+const interests = [
+  {
+    title: 'Tacos',
+    icon: '<i class="fa-regular fa-taco fa-4x"></i>'
+  }, 
+  {title: "Brazilian Jiujitsu",
+  icon: '<i class="fa-regular fa-uniform-martial-arts fa-4x"></i>'
+  },
+  {
+    title: 'Flying Airplanes',
+    icon: '<i class="fa-regular fa-plane-prop fa-4x"></i>'
+  },
+  {
+    title: 'Traditional Archery',
+    icon: '<i class="fa-regular fa-bow-arrow fa-4x"></i>'
+  },
+  {
+    title: "Drone Photography/Videography",
+    icon: '<i class="fa-regular fa-drone-front fa-4x"></i>'
+  },
+  {
+    title: "Fly Fishing",
+    icon: '<i class="fa-regular fa-fishing-rod fa-4x"></i>'
+  },
+  {
+    title: "Organic Gardening",
+    icon: '<i class="fa-regular fa-carrot fa-4x"></i>'
+  },
+  {
+    title: "Wood-Fired Pizza",
+    icon: '<i class="fa-regular fa-pizza-slice fa-4x"></i>'
+  }
+
+]
---
+++ js/work.js
@@ -0,0 +1,8 @@
+const work = [
+  {
+    title: "Tic Tac Toe",
+    description: "A simple, responsive browser game utilizing Client-Side MVC Architecture.",
+    url: "https://razorhollowttt.netlify.app/",
+    screenshot: "../assets/tttScreenshot.png"
+  }
+]
