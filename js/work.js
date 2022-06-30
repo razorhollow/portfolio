a867fcb4596ca5a8777a64a60021e95fe505aa8e
RazorHollow <rob@razorhollow.com>
2022-06-30 12:47:41 -0400

    add new portfolio piece to work section: gas-pass

--- js/work.js
+++ js/work.js
@@ -5,6 +5,13 @@ const work = [
     url: "https://razorhollowttt.netlify.app/",
     github: "https://github.com/razorhollow/ttt-weekend",
     screenshot: "../assets/tttScreenshot.png"
+  },
+  {
+  title: "Gas Pass",
+  description: "An automated employee fuel allowance system utilizing MEN stack and google OAth",
+  url: "https://rmi-gas-pass.herokuapp.com",
+  github: "https://github.com/razorhollow/gas-pass",
+  screenshot: "../assets/gas-pass.png"
   }
 ]
 
