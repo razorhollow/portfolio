f1165073712fd5de5038b9015c544e7752227d62
RazorHollow <rob@razorhollow.com>
2022-08-12 09:45:54 -0400

    add jitsplanner to work section and rearrange order

--- js/work.js
+++ js/work.js
@@ -1,17 +1,17 @@
 const work = [
   {
-    title: "Tic Tac Toe",
-    description: "A simple, responsive browser game utilizing Client-Side MVC Architecture.",
-    url: "https://razorhollowttt.netlify.app/",
-    github: "https://github.com/razorhollow/ttt-weekend",
-    screenshot: "../assets/tttScreenshot.png"
+    title: "JitsPlanner",
+    description: "Lesson planning software that automatically generates weekly lesson plans based on a queue of techniques.",
+    url: "https://jits-planner.herokuapp.com",
+    github: "https://github.com/razorhollow/jits-lesson",
+    screenshot: "../assets/jitsplanner.jpg"
   },
   {
-  title: "Gas Pass",
-  description: "An automated employee fuel allowance system utilizing MEN stack and google OAth",
-  url: "https://rmi-gas-pass.herokuapp.com",
-  github: "https://github.com/razorhollow/gas-pass",
-  screenshot: "../assets/gas-pass.png"
+    title: "Gas Pass",
+    description: "An automated employee fuel allowance system utilizing MEN stack and google OAth",
+    url: "https://rmi-gas-pass.herokuapp.com",
+    github: "https://github.com/razorhollow/gas-pass",
+    screenshot: "../assets/gas-pass.png"
   },
   {
     title: "Squad Swap",
@@ -19,6 +19,13 @@ const work = [
     url: "https://squad-swap.netlify.app/",
     github: "https://github.com/egger0a6/squad-swap",
     screenshot: "../assets/squad-swap.png"
+  },
+  {
+    title: "Tic Tac Toe",
+    description: "A simple, responsive browser game utilizing Client-Side MVC Architecture.",
+    url: "https://razorhollowttt.netlify.app/",
+    github: "https://github.com/razorhollow/ttt-weekend",
+    screenshot: "../assets/tttScreenshot.png"
   }
 ]
 
